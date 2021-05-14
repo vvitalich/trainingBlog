3a27d5d4565487593dee292bbbf539f524b34d1b
vvitalich <vvitalich@ukr.net>
2021-05-14 20:18:30 +0300

    Editing and deleting posts

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, verbose_name='Название' )
     author = models.ForeignKey(
         'auth.User',
-        on_delete=models.CASCADE,
+        on_delete=models.CASCADE, verbose_name='Автор'
     )
-    body = models.TextField()
+    body = models.TextField(verbose_name='Текст')
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
